efb2c8391c58352d8864d6e247d6eaf05166a692
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-17 06:36:54 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char abc;
+	for (abc = 'a'; abc <= 'z'; abc++)
+	putchar(abc);
+	putchar('\n');
+	retun (0);
+}
