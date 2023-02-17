608826cf8c80a1f34797b1ce0e473dafdb34dba3
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-17 10:12:19 -0800

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+	char cba;
+
+	for (cba = 'z'; cba >= 'a'; cba--)
+		putchar(cba);
+	putchar('\n')
+
+	return (0);
+}
