04373cc3e06e45b2ffb556884e6a785cd391ed98
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-17 08:56:13 -0800

    Print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 	char abc;
 
 	for (abc = 'a'; abc <= 'z'; abc++)
-		putchar(abc);
+	putchar(abc);
 	for (abc = 'A'; abc <= 'Z'; abc++)
-		putchar(abc);
-		putchar('\n');
+	putchar(abc);
+	putchar('\n');
 	return (0);
 }		
