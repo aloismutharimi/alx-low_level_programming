4b58a7beaf286094ccba7ae290b2bcdcca48030c
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-17 09:08:52 -0800

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib>
+/*
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char abc
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+	if ((abc != 'e') (abc != 'e'))
+	putchar(abc);
+	}
+	putchar('\n')
+	return (0);
+}
