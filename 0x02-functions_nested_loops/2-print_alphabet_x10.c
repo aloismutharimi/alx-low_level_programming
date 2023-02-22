341fe92459be70478fbe541b0323030c281adef7
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-22 03:10:24 -0800

    Is lower?

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphebet 10 times in lowercase, followed by new line
+ * print_alphabet_x10 - Prints the alphebet 10 times in lowercase
  * Return: 0 (Success)
  */
 void print_alphabet_x10(void)
@@ -10,6 +10,7 @@ void print_alphabet_x10(void)
 	while (count < 10)
 	{
 		char abc;
+
 		for (abc = 'a'; abc <= 'z'; abc++)
 		{
 			_putchar(abc);
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Checks whether character is lowercase
+ * @c: Character to check
+ * Return: 1 if 'c' is lowercase, otherwise return 0
+ */
+int _is lower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}	
