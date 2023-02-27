9478bb6bea7b2c2c361dd4abdd31ab8bc1917670
Alois Mutharimi <aloismutharimi@gmail.com>
2023-02-27 04:05:55 -0800

    Another one

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int str = 0;
 
-	while (*(s + large) != '\0')
+	while (*(s + str) != '\0')
 	{
 		str++;
 	}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by new line
+ * *str: Given string
+ */
+
+void _puts(char *str);
+{
+	int msg;
+
+	for (msg = 0; *(str + msg) != '\0'; msg++)
+	{
+		_putchar(str[msg]);
+	}
+	_putchar('\n');
+}
