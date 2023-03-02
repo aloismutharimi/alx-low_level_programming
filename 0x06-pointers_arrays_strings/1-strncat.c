8446552cffc653013cf3b5999dd6e3f6ccdc4996
Alois <aloismutharimi@gmail.com>
2023-03-02 14:33:19 +0300

    Last commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - Entry point.
+ * _strncat - Entry point.
  * @dest: Variable which will hold the string.
  * @src: Variable which holds the string.
  *@n: int
@@ -8,29 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	char *destPointer = dest;
-	int len = 0;
-<<<<<<< HEAD
+	int i = 0;
+	int j = 0;
 
-=======
-	
->>>>>>> 3c1083e98b5ae9753ad507965c407c65cd043487
-	if (dest == 0)
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != src[n])
 	{
-		return (0);
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	/*Lenght of string dest*/
-	while (*dest)
-	{
-		len++;
-		dest++;
-	}
-	dest = destPointer;
-	for (i = 0; i < n && src[i]; i++)
-	{
-		dest[len + i] = src[i];
-	}
-	dest[len + i] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }
