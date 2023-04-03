2cca969aa7e1432b1bf4c67316567169021d4f16
Alois <aloismutharimi@gmail.com>
2023-04-03 22:15:08 +0300

    Initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints elements of a list
+ * @h: List
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint *h)
+{
+	size_t count_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		
+		count_nodes += 1;
+		h = h->next;
+	}
+	return (count_nodes);
+}
