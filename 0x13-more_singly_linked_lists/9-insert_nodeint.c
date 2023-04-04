8d4b51c7788e3062a0a0047fc5535991d0d1d4f3
Alois <aloismutharimi@gmail.com>
2023-04-04 21:13:15 +0300

    New commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,39 +8,45 @@
  * Return: Address of new node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, intn)
 {
 	unsigned int i = 0;
-	listint_t *new_node, *temp;
+	listint_t *new_node, *tmp;
 
+	/* account for empty pointer */
 	if (head == NULL)
 		return (NULL);
 
+	/* malloc and set values for new node */
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 
-	if (idx == 0);
+	/* account for idx 0 */
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	temp = *head;
+	/* iterate to 1 before nth index to insert */
+	tmp = *head;
 	while (i < (idx - 1))
 	{
-		temp = temp->next;
+		tmp = tmp->next;
 		i++;
 
-		if (temp == NULL)
+		if (tmp == NULL) /* account for idx out of range */
 		{
 			free(new_node);
 			return (NULL);
 		}
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
+
+	/* link into list */
+	new_node->next = tmp->next;
+	tmp->next = new_node;
 	return (new_node);
 }
