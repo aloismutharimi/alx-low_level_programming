095b206a1db2f423def8811428b3e339695b623b
Alois <aloismutharimi@gmail.com>
2023-04-04 21:20:06 +0300

    New

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  * Return: Address of new node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, intn)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *new_node, *tmp;
